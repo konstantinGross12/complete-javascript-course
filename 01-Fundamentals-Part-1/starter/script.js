396e8b5983dd6900420cd62d43fbf6279d21163f
Konstantin Gross <gross.konstantin.1987@gmail.com>
2024-04-08 00:42:34 +0200

    cmmint

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,14 +1,20 @@
-let massMark = 78;
-let heightMark = 1.69;
-let massJohn = 92;
-let heightJohn = 1.95;
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
 
-let BMIMark;
-let BMIJohn;
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
 
-BMIMark = massMark / (heightMark * heightMark);
-BMIJohn = massJohn / (heightJohn * heightJohn);
+/* Write your code below. Good luck! 🙂 */
 
 let markHigherBMI = BMIMark > BMIJohn;
+let johnHigherBMI = BMIJohn > BMIMark;
+if (markHigherBMI) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+}
 
-console.log(BMIMark, BMIJohn, markHigherBMI);
+if (johnHigherBMI) {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
