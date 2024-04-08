a7d4731e9b3855413415f90817a1a79a787826db
Konstantin Gross <gross.konstantin.1987@gmail.com>
2024-04-08 22:36:07 +0200

    part 2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,20 +1,7 @@
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
-
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn);
+const bill = 275;
 
 /* Write your code below. Good luck! 🙂 */
 
-let markHigherBMI = BMIMark > BMIJohn;
-let johnHigherBMI = BMIJohn > BMIMark;
-if (markHigherBMI) {
-  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
-}
+let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-if (johnHigherBMI) {
-  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
-}
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
