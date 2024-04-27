74cfe01d9d456fabb0b1cc694078461489c2a8d9
Konstantin Gross <gross.konstantin.1987@gmail.com>
2024-04-27 15:21:57 +0200

    o9

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,14 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(starterIndex, mainIndex, time, address);
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +35,41 @@ const restaurant = {
     },
   },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+// Destructuring Objects
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+//console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+
+// console.log(menu, starters);
+
+// Mutating Variables
+
+let a = 111;
+let b = 999;
+// console.log(a, b);
+// const obj = { a: 23, b: 7, c: 14 };
+
+// ({ a, b } = obj);
+
+// console.log(a, b);
+
+const {
+  fri: { open, close },
+} = hours;
+
+// console.log(open, close);
