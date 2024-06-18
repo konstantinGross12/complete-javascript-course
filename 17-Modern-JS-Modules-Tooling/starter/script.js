86298f83f0fdd8493426b4065737ae2b47812c16
Konstantin Gross <gross.konstantin.1987@gmail.com>
2024-06-18 20:26:37 +0200

    changes

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,8 @@
+// Importing module
+// import { addToCart, totalPrice as price, tq } from './shoppingCard.js';
+
+import * as ShoppingCart from './shoppingCard.js';
+
+console.log('Importing module');
+
+ShoppingCart.addToCart('bread', 5);
