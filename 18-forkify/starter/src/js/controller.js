7fbaa65473bee2b3218ca2a77aa6e0a2b5714eba
Konstantin Gross <gross.konstantin.1987@gmail.com>
2024-06-21 22:05:10 +0200

    MCV started

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,4 +1,10 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+// import icons from '../img/icons.svg'; // Parcel 1
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +17,25 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1. Loading recipe
+    await model.loadRecipe(id);
+
+    // 2. Rendering recipe
+
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
